67d00f8f3318fd37937377b1ecafc9709cfa0589
skiym-aaa <y.sakiyama3216@gmail.com>
2020-02-24 20:58:04 +0900

    [Update] routing

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
   attachment :profile_image
+  # deviseでemailを不必要にする
+  def email_required?
+     false
+  end
+  def email_changed?
+     false
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_053105) do
+ActiveRecord::Schema.define(version: 2020_02_24_103102) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2020_02_24_053105) do
     t.datetime "updated_at", null: false
     t.string "introduction"
     t.string "profile_image_id"
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
