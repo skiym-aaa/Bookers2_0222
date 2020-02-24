ed27fd18065c961f878904d39ac44b7c6ebba0f3
skiym-aaa <y.sakiyama3216@gmail.com>
2020-02-24 13:32:38 +0900

    [Start]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,43 @@
+class BooksController < ApplicationController
+  def index
+      @books= Book.all
+      @book= Book.new
+  end
+
+  def create
+      book = Book.new(book_params)
+      if book.save
+        # 投稿に成功した場合
+        flash[:notice] = 'You have created book successfully.'
+        redirect_to book_path(book.id)
+      else
+        books = Book.all
+        render :index
+      end
+  end
+
+  def show
+      @book = Book.find(params[:id])
+  end
+
+  def edit
+      @book = Book.find(params[:id])
+  end
+
+  def update
+      book = Book.find(params[:id])
+      if book.update(book_params)
+        # 編集に成功した場合
+        flash[:notice] = 'You have updated book successfully.'
+        redirect_to book_path(book.id)
+      else
+        render :edit
+      end
+  end
+
+  def destroy
+      book = Book.find(params[:id])
+      book.destroy
+      redirect_to books_path
+  end
+end
