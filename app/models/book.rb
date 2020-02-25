b6acdb0e50c36eca59e16ac428ce33657a67dbf1
skiym-aaa <y.sakiyama3216@gmail.com>
2020-02-25 09:14:15 +0900

    [Add] flash

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user, optional: true
-  validates :title, presence: true
-  validates :body, presence: true
+  validates :title,presence: true
+  validates :body,presence: true
+  validates :body,length: { minimum: 1, maximun:200 }
 end
